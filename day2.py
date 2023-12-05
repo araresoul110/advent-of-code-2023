08f893b1b5e1f9fc6d4263b51f4008c46d783614
araresoul110 <119128783+araresoul110@users.noreply.github.com>
2023-12-05 10:55:48 -0600

    Day 2 part 2 complete

--- day2.py
+++ day2.py
@@ -1,32 +1,48 @@
-# Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 from prep_input import *
 
-def find_valid_games():
-    id_numbers = []
-    criterion_dict ={"red": 12, "green": 13, "blue": 14}
-
+def process_games(part):
+    numbers = []
     Lines = prep_input('day2_input.txt')
 
     for line in Lines:
         game_id = line[:line.index(":")].replace("Game", "")
         split_line = line.replace(":", ";").split(";")
+        if part == 1:
+            if find_valid_games(split_line):
+                numbers.append(int(game_id))
+        else:
+            numbers.append(find_minimum_cubes(split_line))
+    
+    return numbers
+        
 
-        valid = True
-        for handful in split_line[1:]:
-            values = [int(i) for i in handful.split() if i.isdigit()]
-            colors = [j.replace(",", "") for j in handful.split() if j.isdigit() == False]
-            for x in range(len(colors)):
-                if values[x] > criterion_dict[colors[x]]:
-                    valid = False
-                    break
+def find_valid_games(split_line):
+    criterion_dict ={"red": 12, "green": 13, "blue": 14}
 
-        if valid == True:
-            id_numbers.append(int(game_id))
+    valid = True
+    for handful in split_line[1:]:
+        values = [int(i) for i in handful.split() if i.isdigit()]
+        colors = [j.replace(",", "") for j in handful.split() if j.isdigit() == False]
+        for x in range(len(colors)):
+            if values[x] > criterion_dict[colors[x]]:
+                valid = False
+                break
 
-    return id_numbers
+    return valid
 
-numbers = find_valid_games()
+def find_minimum_cubes(split_line):
+    minimum_dict ={"red": 0, "green": 0, "blue": 0}
+    for handful in split_line[1:]:
+        values = [int(i) for i in handful.split() if i.isdigit()]
+        colors = [j.replace(",", "") for j in handful.split() if j.isdigit() == False]
+        for x in range(len(colors)):
+            if values[x] > minimum_dict[colors[x]]:
+                minimum_dict[colors[x]] = values[x]
+    return minimum_dict["red"] * minimum_dict["blue"] * minimum_dict["green"]
+
+numbers = process_games(2)
 solution = sum(numbers)
+
 print(solution)
 # Solution:
 #  2512
